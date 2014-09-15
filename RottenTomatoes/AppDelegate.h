8fb63792f1c4d63376b58df8c4adda28af31d6dd
Jim Tao <jianbao@yahoo-inc.com>
2014-09-14 17:38:55 -0700

    init commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Jianbao Tao on 9/12/14.
+//  Copyright (c) 2014 ___JimTao___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RottenTomatoes/MovieTitleViewController.h
@@ -0,0 +1,13 @@
+//
+//  MovieTitleViewController.h
+//  RottenTomatoes
+//
+//  Created by Jianbao Tao on 9/12/14.
+//  Copyright (c) 2014 ___JimTao___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieTitleViewController : UIViewController
+
+@end
